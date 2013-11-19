6609b61ede16c805489aaeaa70bcceab8b3ed686
hank chiu <hank chiu@hank>
2013-11-19 15:28:37 +0800

    pull

--- testpro1/src/com/example/testpro1/MainActivity.java
+++ testpro1/src/com/example/testpro1/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Log.d("", "hank test testttretert");
+		Log.d("", "hank test");
 		setContentView(R.layout.activity_main);
 	}
 
